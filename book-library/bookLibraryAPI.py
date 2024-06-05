8bd8c877801457ef53121c59af949c40a12b4cfe
Yaakov Seif <129808482+Yseif02@users.noreply.github.com>
2024-06-05 13:27:36 -0400

    simple book library API

---
+++ book-library/bookLibraryAPI.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+class Book(BaseModel):
+    id: Optional[int] = None
+    title: str
+    author: str
+    year: int
+
+
+app = FastAPI()
+
+books = [] 
+
+@app.post("/books/")
+def create_book(book: Book):
+    book.id = len(books) + 1
+    books.append(book)
+    return book, book.id
+
+@app.get("/books/")
+def get_all_books():
+    return books
+
+@app.get("/books/{id}")
+def get_book_by_id(id: int):
+    for book in books:
+        if book.id == id:
+            return {"Title" : book.title, "book" : book}
+    return {"message": "Book not found"}
+
+@app.put("/books/{id}")
+def update_book(id: int, updated_book: Book):
+    for book in books:
+        if book.id == id:
+            book.title = updated_book.title
+            book.author = updated_book.author
+            book.year = updated_book.year
+            return {"message": "Book updated successfully"}
+    return {"message": "Book not found"}
+
+@app.delete("/books/{id}")
+def delete_book(id: int):
+    for book in books:
+        if book.id == id:
+            books.remove(book)
+            return {"message": "Book deleted successfully", "book id": id}
+    return {"message": "Book not found"}
