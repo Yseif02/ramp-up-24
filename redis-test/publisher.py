23fba6212561728be03cf5b85b332b3206508819
Yaakov Seif <129808482+Yseif02@users.noreply.github.com>
2024-06-07 01:22:40 -0400

    working version of redis-test

--- redis-test/publisher.py
+++ redis-test/publisher.py
@@ -8,4 +8,5 @@ r = redis.Redis(host='redis-server', port=6379)
 @app.post("/publish{message}")
 async def publish_message(request: Request, message: str):
     r.publish('my-channel', message)
-    return {"status": "Message published successfully"}
+    return {"status": "Message published successfully", "message": message}
+    
